af51492815fc2eca6115286d647b3a0654c9d5bf
prashant-v-223 <prashantvadhvana@gmail.com>
2024-08-01 18:22:36 +0530

    sdf

--- app/page.tsx
+++ app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
                   src="images/Group 1000004870.png"
                   loading="lazy"
                   alt=""
-                  className=""
+                  className="w-[50%]"
                 />
               </a>
               <nav
